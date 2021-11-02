842034ec081b0c6c8852b4045730de4c627a10e7
akshayakula <akshayakula@vt.edu>
2021-11-01 21:21:40 -0400

    create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
 
+    mapping(uint => Task) tasks;
+    
 }
 
